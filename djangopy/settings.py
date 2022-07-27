e51427c1dbe0d7e4541484f12b391de1a1ef44f4
Mark Philip <markphd@pm.me>
2022-07-28 00:06:21 +0400

    use default settings

--- djangopy/settings.py
+++ djangopy/settings.py
@@ -122,7 +122,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
@@ -130,9 +130,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static"
+# ]
