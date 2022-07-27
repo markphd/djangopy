0ebc484ff831bf480d0924e2d0dd51a98ca62770
Mark Philip <markphd@pm.me>
2022-07-28 00:09:57 +0400

    update urls to use static root

--- djangopy/urls.py
+++ djangopy/urls.py
@@ -18,9 +18,11 @@ from django.urls import include, path
 
 from app.views import views
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('app.urls')),
     path('admin/', admin.site.urls),
     path('views', views)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
